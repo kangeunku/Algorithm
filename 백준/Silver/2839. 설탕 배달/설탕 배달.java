2264c26945f8f59a3a8e18a33495627aed2fa451
EunKu <106164235+kangeunku@users.noreply.github.com>
2023-11-03 10:26:42 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,41 +2,26 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-import java.util.Queue;
-import java.util.LinkedList;
-
 public class Main{
-    static Queue<Integer> Q = new LinkedList<>();
-    static int[] arr = new int[5010];
-    static int[] sugar = {3,5};
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        System.out.print(def(N));
-
-    }
-    public static int def(int N){
-        int start = 0;
-        int count = 1;
-        Q.offer(start);
-        while(!Q.isEmpty()){
-            int len = Q.size();
-            for (int i = 0; i <len; i++){
-                int temp = Q.poll();
-                for(int a : sugar){
-                    if(temp + a == N){
-                        return count;
-                    }
-                    if(arr[temp + a] == 0 && temp+a < 5005){
-                        arr[temp + a] = count;
-                        Q.offer(temp + a);
-                    }
-                }
-
-            }
-            count++;
+        int sugar = Integer.parseInt(br.readLine());
+        int count = 0;
+        while(sugar >0){
+        if(sugar % 5 == 0){
+            count += sugar / 5;
+            System.out.print(count);
+            return;
+     
         }
-        return -1;
+        if(sugar < 3){
+            System.out.print(-1);
+            return;
+        }
+        sugar -= 3;
+        count++;    
+    }
+    System.out.print(count);
     }
 }
