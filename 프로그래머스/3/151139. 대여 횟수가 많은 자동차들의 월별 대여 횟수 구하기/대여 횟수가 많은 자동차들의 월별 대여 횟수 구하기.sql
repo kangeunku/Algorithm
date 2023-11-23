29a045ac702c63eaef87a320172638873a88f3a1
EunKu <106164235+kangeunku@users.noreply.github.com>
2023-11-23 18:17:35 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,16 +1,10 @@
-select extract(month from START_DATE) as month, car_id, count(history_id) as records
-from
-    CAR_RENTAL_COMPANY_RENTAL_HISTORY
-where TO_CHAR(START_DATE, 'YYYYMMDD') BETWEEN 20220801 AND 20221031
-and car_id in(
-SELECT CAR_ID
-FROM
-    CAR_RENTAL_COMPANY_RENTAL_HISTORY
-WHERE TO_CHAR(START_DATE, 'YYYYMMDD') BETWEEN 20220801 AND 20221031 
-GROUP BY
-    CAR_ID
-having count(*) >=5)
-group by
-    car_id, extract(month from START_DATE)
-order by
-    extract(month from START_DATE) asc, car_id desc
+select to_number(to_char(a.start_date,'mm')) as month, a.car_id, count(*) as records 
+    from car_rental_company_rental_history a inner join (
+        select car_id from car_rental_company_rental_history
+        where to_char(start_date,'yymm')>='2208'and to_char(start_date,'yymm')<='2210'
+        group by car_id
+        having count(*) >=5) b
+    on a.car_id = b.car_id 
+where to_char(a.start_date,'yymm')>='2208'and to_char(a.start_date,'yymm')<='2210'
+group by  to_number(to_char(a.start_date,'mm')), a.car_id 
+order by month,car_id desc;
