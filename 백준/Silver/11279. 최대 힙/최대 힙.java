4f2e8f5c56b2b7eb2fefdc98c7511cf8f1bbe429
EunKu <106164235+kangeunku@users.noreply.github.com>
2023-11-26 22:41:39 +0900

    [Silver II] Title: 최대 힙, Time: 1096 ms, Memory: 109812 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
         int n = sc.nextInt();
         PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
         for (int i = 0; i < n; i++) {
             int temp = sc.nextInt();
             if (temp !=0) pQ.offer(temp);
             else if(!pQ.isEmpty()){
-                System.out.println(pQ.poll());
-            } else System.out.println(0);
+                sb.append(pQ.poll()).append("\n");
+            } else sb.append(0).append("\n");
         }
+        System.out.println(sb);
     }
 }
