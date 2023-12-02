db02fd78828bb9289d3cf57b9ed1ed7fe52162cb
EunKu <106164235+kangeunku@users.noreply.github.com>
2023-12-02 17:21:33 +0900

    [Silver III] Title: N과 M (2), Time: 212 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    static int n,m;
+    static boolean[] check;
+    static int[] list;
+    static StringBuilder sb;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        check = new boolean[n];
+        list = new int[m];
+        sb = new StringBuilder();
+        sol(0, 0);
+        System.out.println(sb);
+    }
+    static void sol(int depth, int start){
+        if (depth == m){
+            for (int i=0; i<m; i++) {
+                sb.append(list[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            if(!check[i]){
+                list[depth] = i+1;
+                sol(depth+1, i +1);
+            }
+
+
+        }
+    }
+}
